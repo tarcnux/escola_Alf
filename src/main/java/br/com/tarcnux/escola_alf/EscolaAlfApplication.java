56c3375828da44783dde4079d0c7bdfea16685bd
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-31 18:23:11 -0300

    Initial project

---
+++ src/main/java/br/com/tarcnux/escola_alf/EscolaAlfApplication.java
@@ -0,0 +1,13 @@
+package br.com.tarcnux.escola_alf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaAlfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaAlfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tarcnux/escola_alf/EscolaAlfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tarcnux.escola_alf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaAlfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
