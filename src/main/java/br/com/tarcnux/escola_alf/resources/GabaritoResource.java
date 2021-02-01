f69a3eba1518bb028095a9d8cc7ca0480d599baa
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-01 05:35:52 -0300

    PUT atualizar Gabarito com tratamento de excessão

--- src/main/java/br/com/tarcnux/escola_alf/resources/GabaritoResource.java
+++ src/main/java/br/com/tarcnux/escola_alf/resources/GabaritoResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,4 +58,13 @@ public class GabaritoResource {
 		
 		return ResponseEntity.created(uri).body(dto);
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<GabaritoDTO> update(@PathVariable Long id, @RequestBody GabaritoDTO dto){
+		dto = gabaritoService.update(id,dto);
+		
+		
+		return ResponseEntity.ok().body(dto);
+	}
+	
 }
--- src/main/java/br/com/tarcnux/escola_alf/services/GabaritoService.java
+++ src/main/java/br/com/tarcnux/escola_alf/services/GabaritoService.java
@@ -2,6 +2,8 @@ package br.com.tarcnux.escola_alf.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -64,5 +66,39 @@ public class GabaritoService {
 		entity = gabaritoRepository.save(entity);
 		
 		return new GabaritoDTO(entity);
+	}
+
+	@Transactional
+	public GabaritoDTO update(Long id, GabaritoDTO dto) {
+		try {
+			Gabarito entity = gabaritoRepository.getOne(id);
+			entity.setNome(dto.getNome());
+			entity.setPeso01(dto.getPeso01());
+			entity.setPeso02(dto.getPeso02());
+			entity.setPeso03(dto.getPeso03());
+			entity.setPeso04(dto.getPeso04());
+			entity.setPeso05(dto.getPeso05());
+			entity.setPeso06(dto.getPeso06());
+			entity.setPeso07(dto.getPeso07());
+			entity.setPeso08(dto.getPeso08());
+			entity.setPeso09(dto.getPeso09());
+			entity.setPeso10(dto.getPeso10());
+			entity.setResposta01(dto.getResposta01());
+			entity.setResposta02(dto.getResposta02());
+			entity.setResposta03(dto.getResposta03());
+			entity.setResposta04(dto.getResposta04());
+			entity.setResposta05(dto.getResposta05());
+			entity.setResposta06(dto.getResposta06());
+			entity.setResposta07(dto.getResposta07());
+			entity.setResposta08(dto.getResposta08());
+			entity.setResposta09(dto.getResposta09());
+			entity.setResposta10(dto.getResposta10());
+			
+			entity = gabaritoRepository.save(entity);
+			return new GabaritoDTO(entity);
+			
+			} catch(EntityNotFoundException e) {
+				throw new ResourceNotFoundException(String.format("Id %d não encontrado", id));
+			}
 	}
 }
